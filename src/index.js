c473e2e995a2f3ddf8292253685767dd776f67f3
gazdagergo <info@gazdagergo.com>
2018-11-15 15:34:46 +0100

    pies

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const d3 = {
   shape
 };
 
+const Svg = ({ children }) => (
+  <svg x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100">
+    <g transform={`translate(${x},${y})`}>{children}</g>
+  </svg>
+);
+
 const size = 100;
 const width = size;
 const height = size;
@@ -16,26 +22,18 @@ const Arc = ({
   size,
   outerRadius = 40,
   innerRadius = 30,
-  startAngleRad = 0,
-  endAngleRad = 4
+  startAngle = 0,
+  endAngle = 4
 }) => {
   const arcGenerator = d3.shape
     .arc()
     .outerRadius(outerRadius)
     .innerRadius(innerRadius)
-    .startAngle(startAngleRad)
-    .endAngle(endAngleRad);
+    .startAngle(startAngle)
+    .endAngle(endAngle);
 
   return <path stroke="#FFFFFF" d={arcGenerator()} />;
 };
-
-const Svg = () => (
-  <svg x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100">
-    <g transform={`translate(${x},${y})`}>
-      <Arc />
-    </g>
-  </svg>
-);
 
 const data = [
   { number: 4, name: "Locke" },
@@ -46,10 +44,17 @@ const data = [
   { number: 42, name: "Kwon" }
 ];
 
-const arcs = d3.shape.pie().value(function(d) {
-  return d.number;
-})(data);
-const paths = arcs.map(arc => <Arc {...arc} />);
+const Pies = ({ data }) => {
+  const arcs = d3.shape.pie().value(function(d) {
+    return d.number;
+  })(data);
+  return arcs.map(arc => <Arc {...arc} />);
+};
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Svg />, rootElement);
+ReactDOM.render(
+  <Svg>
+    <Pies data={data} />
+  </Svg>,
+  rootElement
+);
